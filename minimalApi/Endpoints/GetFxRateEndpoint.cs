803eb37826cd0892bbecee81aac0ce60867352b0
bojan_pantovic <pantovic1989@yahoo.com>
2022-03-06 17:52:05 +0100

    initial commit

---
+++ minimalApi/Endpoints/GetFxRateEndpoint.cs
@@ -0,0 +1,60 @@
+﻿using minimalApi.Configuration;
+using minimalApi.Database;
+using System.Data;
+using System.Data.Common;
+
+namespace minimalApi.Endpoints;
+
+public static class GetFxRateEndpoint
+{
+
+    public static void AddGetAllFxRatesEndpoint(this WebApplication? app, WebApplicationBuilder builder)
+    {
+        if (app == null) return;        
+
+        var appSettings = builder.Configuration.Get<AppSettings>();
+        app.MapGet("/fxRate", [Authorize] async (DbProviderFactory dbProviderFactory, HttpResponse response) =>
+        {
+            using var db = dbProviderFactory.CreateConnection();
+            db.ConnectionString = appSettings.ConnectionString;
+            await db.OpenAsync();
+
+            using var cmd = CreateReadCommand(db);
+
+            var result = ReadResult(cmd);
+            await response.WriteAsJsonAsync(result);
+            return;
+        })
+        .Produces(StatusCodes.Status200OK);
+    }    
+
+    static DbCommand CreateReadCommand(DbConnection connection)
+    {
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT currency_code, rate FROM v_currencies";
+
+        (cmd as MySqlConnector.MySqlCommand)?.Prepare();
+
+        return cmd;
+    }
+
+    static async Task<List<FxRate>> ReadResult(DbCommand cmd)
+    {
+        var result = new List<FxRate>();
+
+        using (var rdr = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection))
+        {
+            await rdr.ReadAsync();
+            while (await rdr.ReadAsync())
+            {
+                result.Add(new FxRate()
+                {
+                    CurrencyCode = rdr.GetString(0),
+                    Rate = rdr.GetString(1),
+                });
+            }
+        }
+        return result;
+    }
+}
+
---
+++ minimalApi/Program.cs
@@ -0,0 +1,57 @@
+
+using minimalApi.Configuration;
+using minimalApi.Database;
+using minimalApi.Endpoints;
+using MySqlConnector;
+using System.Data.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<AppSettings>(builder.Configuration);
+
+builder.Services.AddSingleton<DbProviderFactory>(MySqlConnectorFactory.Instance);
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.Use(async (ctx, next) =>
+{
+    try
+    {
+        await next();
+    }
+    catch (BadHttpRequestException ex)
+    {
+        ctx.Response.StatusCode = ex.StatusCode;
+        await ctx.Response.WriteAsync(ex.Message);
+    }
+});
+
+app.UseRouting();
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.AddLoginEndpoint(builder);
+app.AddGetAllFxRatesEndpoint(builder);
+
+app.Run();
